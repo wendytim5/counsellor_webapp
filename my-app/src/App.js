a989b58eb8fe563a456a1df7fea61942b5fcd89a
wendytim5 <wendy@myladder.africa>
2024-07-08 01:33:28 +0000

    edit backend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import LandingPage from './components/LandingPage';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
-import Dashboard from './pages/dashboard';
-import CreateStudent from './pages/CreateStudent';
-import FirstDashboard from './pages/FirstDashboard';
-import ViewStudent from './pages/ViewStudent';
-import HomePage from './pages/HomePage';
-import Notes from './components/Notes';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/createstudent" element={<CreateStudent/>} />
-          <Route path="/firstdashboard" element={<FirstDashboard/>} />
-          <Route path="/viewstudent" element={<ViewStudent/>} />
-          <Route path="/homepage" element={<HomePage/>} />
-          <Route path="/notes" element={<Notes/>} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
